f8016b7961aecda5f1f383166d0e9710934842a2
RavinduHasithanjana <ravinduceylofana@outlook.com>
2021-08-07 23:40:45 +0530

    added changes to cros issue

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,6 +2,19 @@ module.exports = function (app, multer, path) {
   var cors = require("cors");
   app.use(cors());
 
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    next();
+  });
+
   const products = require("../controller/Product.controller");
 
   var storage = multer.diskStorage({
