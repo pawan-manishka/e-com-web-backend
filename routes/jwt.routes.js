c6c9429999179127a63c2e2579ec1799584c9aa0
RavinduHasithanjana <ravinduceylofana@outlook.com>
2021-08-08 19:28:07 +0530

    added cros to jwt

--- routes/jwt.routes.js
+++ routes/jwt.routes.js
@@ -1,7 +1,24 @@
 const verifySignUp = require("./verifySignUp");
 const authJwt = require("./verifyJwtToken");
 
+
 module.exports = function (app) {
+  var cors = require("cors");
+  app.use(cors());
+
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    next();
+  });
+  
   const controller = require("../controller/controller");
 
   app.post(
