a6545f01851e49cac97e8a4798d25b99ad276dde
RavinduHasithanjana <ravinduceylofana@outlook.com>
2021-08-19 23:53:51 +0530

    added changes

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,6 +2,8 @@ module.exports = function (app, multer, path) {
   var cors = require("cors");
   app.use(cors());
 
+  const products = require("../controller/Product.controller");
+
   app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header(
@@ -15,7 +17,7 @@ module.exports = function (app, multer, path) {
     next();
   });
 
-  const products = require("../controller/Product.controller");
+
 
   var storage = multer.diskStorage({
     destination: (req, file, cb) => {
