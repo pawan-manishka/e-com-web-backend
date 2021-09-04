2c63722370a97af98c6d01b4e4b2b32ffd31bfd2
RavinduHasithanjana <ravinduceylofana@outlook.com>
2021-09-04 19:03:04 +0530

    added routes changes

---
+++ routes/admin.order.confirm.route.js
@@ -0,0 +1,26 @@
+module.exports = (app) => {
+    const mail = require("../controller/order.company.controller");
+    var router = require("express").Router();
+    const cors = require("cors");
+  
+    app.use(cors());
+  
+    app.use(function (req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+      );
+      next();
+    });
+  
+    router.post("/", mail.mail);
+  
+    app.use("/api/admin_email", router);
+    
+  };
+  
